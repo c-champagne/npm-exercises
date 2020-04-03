9c99b84b03d55fd5586b6ab5c371a423f144b59c
c-champagne <cassielchampagne@gmail.com>
2020-04-02 23:30:22 -0400

    Initial commit

---
+++ lodashMean.js
@@ -0,0 +1,5 @@
+const _ = require('lodash');
+
+let numbers = [5, 13, 24, 17];
+
+console.log(_.mean(numbers));
---
+++ lodashUniq.js
@@ -0,0 +1,5 @@
+const _ = require("lodash");
+
+let colors = ["red", "blue", "red", "yellow", "green", "blue", "white"];
+
+console.log(_.uniq(colors));
